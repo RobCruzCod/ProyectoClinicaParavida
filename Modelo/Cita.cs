d128fc2e14662aaab02e3cd2869cba5ee002f1d0
BryanJ <mendozajrbryan@gmail.com>
2024-05-28 20:33:21 -0700

    Bryan

---
+++ Modelo/Cita.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ProyectoClinicaParavida.Modelo
+{
+    public class Cita
+    {
+
+    }
+}
---
+++ Modelo/mtoCita.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ProyectoClinicaParavida.Modelo
+{
+    public class mtoCita
+    {
+    }
+}
